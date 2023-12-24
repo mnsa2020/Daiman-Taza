20f8c934482db2fddded9aca61983a99351b6278
Mohamed Ahmed <68556466+mnsa2020@users.noreply.github.com>
2023-12-24 08:12:38 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-// Carousel Multi Items
-let items = document.querySelectorAll('.carousel .carousel-item')
+// Get the button
+let mybutton = document.getElementById("btn-top");
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-// End Carousel Multi Items
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 12000 ||
+    document.documentElement.scrollTop > 12000
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
